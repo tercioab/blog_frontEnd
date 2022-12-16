14c735cb77d5b7a664c0aad55ddec3c7ed966d09
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-16 17:30:47 -0300

    feat: solução html

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,35 +1,23 @@
 import './App.css';
 // import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-
 // import React, { useState, useEffect } from 'react';
+import ReactHtmlParser from 'react-html-parser';
 
 function App() {
-  const [music, setMusic] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch('http://localhost:3000/music');
-      const data = await result.json();
-      setMusic(data);
-    };
-    fetchData();
-  }, []);
+  const content = [
+    { id: 1, text: '<h1> teste </h1> <h2>2ij</h2>' },
+    { id: 2, text: '<h1> teste2 </h1>' },
+    { id: 2, text: '<h1> teste3 </h1>' },
+  ];
   return (
 
     <div>
-      <h1>music</h1>
-      <div>{!music && 'carregando'}</div>
-      <div>
-        {music?.map(({ title, artist }, i) => (
-          <div key={ i }>
-            <h1>{title}</h1>
-            <p>{artist }</p>
-          </div>
+      <h1>teste</h1>
+      {content
+        .map(({ text }) => (ReactHtmlParser(text)
         ))}
-
-      </div>
     </div>
+
   );
 }
 
